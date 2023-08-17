187daecba6f6b39d5bc89bb8d3488cdfe1628fb9
JNICOLASHC <jnherrerac@academia.usbbog.edu.co>
2023-08-17 15:19:36 -0500

    prueba de archivo

---
+++ CORTE_01/Sesion6/iteracion_break.py
@@ -0,0 +1,12 @@
+#jflajdlka
+
+n=int(input('Ingrese un numero:\n'))
+
+i=0
+
+while i<n:
+    i+=1
+    print(i)
+    if i==4:
+        break
+    
---
+++ CORTE_01/Sesion6/iteracion_continue.py
@@ -0,0 +1,11 @@
+#jflajdlka
+
+n=int(input('Ingrese un numero:\n'))
+
+i=0
+
+while i<n:
+    i+=1
+    if i==4:
+        continue
+    print(i)
